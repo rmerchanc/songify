f7c1fe337d0250780e96dcbe544316f5feb577be
Gema Patricia Herrera de Miguel <gp.herrera@usp.ceu.es>
2023-04-13 20:39:26 +0200

    Simulando /artist funcionando

--- songifyAPI/routes/artist.js
+++ songifyAPI/routes/artist.js
@@ -1,8 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/artist', function(req, res, next) {
-  res.send({repuesta: "Hola a todos"});
+router.get('/', function(req, res, next) {
+    res.send([        
+        {
+        "id": 1,
+        "name": "Coldplay",
+        "type": "Group",
+        "area": "United Kingdom",
+        "begin_date": "1996-09",
+        "url_releases": "https://localhost/api/v3/artist/1/releases",
+        "url_recordings": "https://localhost/api/v3/artist/1/recordings"
+        }, 
+        {
+        "id": 2,
+        "name": "John Williams",
+        "type": "Person",
+        "gender": "Male",
+        "area": "Australia",
+        "begin_date": "1941-04-24",
+        "url_releases": "https://localhost/api/v3/artist/2/releases",
+        "url_recordings": "https://localhost/api/v3/artist/2/recordings"
+        }
+    ]);
 });
 
 module.exports = router;
