fd695d05de140dc4da2f58354f49a10d94641021
mariaact <mariactyermo@gmail.com>
2023-05-18 17:23:02 +0200

    canciones

--- songifyAPI/routes/artist.js
+++ songifyAPI/routes/artist.js
@@ -5,28 +5,29 @@ const fetch = require("node-fetch");
 const dbo = require('../db/conn');
 const ObjectId = require('mongodb').ObjectId;
 const MAX_RESULTS = parseInt(process.env.MAX_RESULTS);
-
+const xml2js = require('xml2js');
+const axios = require("axios")
 
 router.get('/', async (req, res) => {
   let limit = MAX_RESULTS;
-  if (req.query.limit){
-    limit =  Math.min(parseInt(req.query.limit), MAX_RESULTS);
+  if (req.query.limit) {
+    limit = Math.min(parseInt(req.query.limit), MAX_RESULTS);
   }
   let next = req.query.next;
   let query = {}
-  if (next){
-    query = {_id: {$lt: new ObjectId(next)}}
+  if (next) {
+    query = { _id: { $lt: new ObjectId(next) } }
   }
   const dbConnect = dbo.getDb();
   let results = await dbConnect
     .collection('artist')
     .find(query)
-    .sort({_id: -1})
+    .sort({ _id: -1 })
     .limit(limit)
     .toArray()
     .catch(err => res.status(400).send('Error to fetch artists'));
   next = results.length == limit ? results[results.length - 1]._id : null;
-  res.json({results, next}).status(200);
+  res.json({ results, next }).status(200);
 });
 
 
@@ -57,9 +58,9 @@ router.get("/", function (req, res, next) {
 });
 
 
-María & Ramón para recuperar datos de la API:
+María & Ramón para recuperar datos de la API:*/
 
-router.get("/:id", function (req, res, next) {
+/*router.get("/:id", function (req, res, next) {
   if (req.params.id) {
     // here should be some type of convertion between our IDs and the API's MBIDs
     console.log("The ID introduced is " + req.params.id);
@@ -91,6 +92,116 @@ router.get("/:id", function (req, res, next) {
     .catch((error) => {
       console.error(error);
     });
+});*/
+
+router.get("/:id", async function (req, res, next) {
+  console.log(req.params.id)
+  const url = `https://musicbrainz.org/ws/2/artist/${req.params.id}?fmt=json`;
+
+  let d = await axios.get(url)
+  let importa = d.data
+  console.log(importa)
+
+  //axius
+  /*fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data.recordings.length)
+      if (data.recordings && data.recordings.length > 0) {
+        data.recordings.forEach(recording => {
+          const title = recording.title;
+          const duration = recording.length / 1000; 
+          const artist = recording["artist-credit"][0].artist.name;
+          console.log(`Title: ${title}, Artist: ${artist}, Duration: ${duration}s`);
+        });
+      } else {
+        console.log("No recordings found for this artist.");
+      }
+    })
+    .catch(error => console.error(error));*/
+
+
+  /* if (req.params.id) {
+     // here should be some type of convertion between our IDs and the API's MBIDs
+     console.log("The ID introduced is " + req.params.id);
+   }
+   const mbid = "74df4e2f-fe75-4b4a-87c5-3f1b4bc95aa2"; // MusicBrainz Identifiers (MBIDs)
+   //const url = `https://musicbrainz.org/ws/2/artist/${mbid}?fmt=json`;
+   
+   const artistId = "74df4e2f-fe75-4b4a-87c5-3f1b4bc95aa2"; 
+   const url = `https://musicbrainz.org/ws/2/artist/${req.params.id}/recordings`;
+   
+   
+   fetch(url)
+   .then(response => response.json())
+   .then(xml => xml2js.parseStringPromise(xml))
+   .then(result => {
+     console.log(result)
+     if (result && result.recordingList && result.recordingList.recording) {
+       const recordings = result.recordingList.recording;
+       recordings.forEach(recording => {
+         const title = recording.title;
+         const artist = recording['artist-credit'][0].name;
+         const duration = recording.length;
+         console.log(`Title: ${title}, Artist: ${artist}, Duration: ${duration}`);
+       });
+     } else {
+       console.log('No recordings found for this artist.');
+     }
+   })
+   .catch(error => console.error(error));*/
 });
-*/
+
+
+router.get("/:id/recordings", async function (req, res, next) {
+
+  const url = `https://musicbrainz.org/ws/2/artist/${req.params.id}/recordings?inc=recordings`
+
+  var datos = [];
+  var objeto = {};
+
+  let d = await axios.get(url)
+  let canciones = d.data
+  
+  for (var i = 0; i < canciones.recordings.length; i++) {
+
+    var nombre = canciones.recordings[i];
+
+    datos.push({
+      "id_recordig": nombre.id,
+      "title": nombre.title,
+      "length": tiempo(nombre.length)
+    });
+  }
+
+  objeto.datos = datos;
+  res.send(datos)
+
+});
+
+router.get("/:id/recording/:id1", async function (req, res, next) {
+
+  const url = `https://musicbrainz.org/ws/2/recording/${req.params.id1}`
+
+  axios.get(url)
+    .then(response => {
+
+      let canciones = response.data
+
+      res.send({
+        "id": canciones.id,
+        "title": canciones.title,
+        "length": tiempo(canciones.length)
+      });
+    });
+});
+
+function tiempo(tiempo) {
+  const segundos = (Math.floor(tiempo / 1000));
+  const minutos = Math.floor(segundos / 60);
+  const seg = (segundos % 60);
+
+  return `${minutos}:${seg}`
+}
+
 module.exports = router;
