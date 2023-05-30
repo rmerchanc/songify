359438529861b3fbfa86954c4c015761180bc2fd
Cristina Balsalobre <c.balsalobre@usp.ceu.es>
2023-05-30 23:36:39 +0200

    Funciona el cliente para todos los métodos y urls

--- songifyAPI/client/client.js
+++ songifyAPI/client/client.js
@@ -1,4 +1,4 @@
-const fetch = require("node-fetch");
+/**const fetch = require("node-fetch");
 // GET request
 fetch('http://localhost:3000/api/v1/artist', {
   method: 'GET'
@@ -9,5 +9,62 @@ fetch('http://localhost:3000/api/v1/artist', {
   })
   .catch(error => {
     console.error('Error:', error);
-  });
+  });*/
 
+  const readline = require('readline');
+  const fetch = require('node-fetch');
+  
+  const fetchUrl = async () => {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+  
+    const method = await new Promise(resolve => {
+      rl.question('Enter the HTTP method (GET, DELETE, PUT, or POST): ', answer => {
+        resolve(answer);
+      });
+    });
+  
+    const url = await new Promise(resolve => {
+      rl.question('Enter the URL: ', answer => {
+        resolve(answer);
+      });
+    });
+  
+    if (method && url) {
+      let requestOptions = {
+        method: method.toUpperCase()
+      };
+  
+      if (method.toUpperCase() === 'PUT' || method.toUpperCase() === 'POST') {
+        const jsonPayload = await new Promise(resolve => {
+          rl.question('Enter the JSON payload: ', answer => {
+            resolve(answer);
+          });
+        });
+  
+        requestOptions.headers = {
+          'Content-Type': 'application/json'
+        };
+  
+        requestOptions.body = jsonPayload;
+      }
+  
+      rl.close();
+  
+      try {
+        const response = await fetch(url, requestOptions);
+        const data = await response.json();
+        console.log('Response:', data);
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    } else {
+      rl.close();
+      console.log('Invalid input');
+    }
+  };
+  
+  fetchUrl();
+  
