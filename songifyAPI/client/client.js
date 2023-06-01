40987d6bab961a3db4371de1d2136c58f3c894f3
Ramón Merchán Castillo <r.merchan@usp.ceu.es>
2023-06-01 23:04:40 +0200

    añadida ruta básica en el cliente

--- songifyAPI/client/client.js
+++ songifyAPI/client/client.js
@@ -1,79 +1,66 @@
-/**const fetch = require("node-fetch");
-// GET request
-fetch('http://localhost:3000/api/v1/artist', {
-  method: 'GET'
-})
-  .then(response => response.json())
-  .then(data => {
-    console.log('GET response:', data);
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });*/
+const readline = require('readline');
+const fetch = require('node-fetch');
 
-  const readline = require('readline');
-  const fetch = require('node-fetch');
-  
-  const fetchUrl = async () => {
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout
+const fetchUrl = async () => {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  const method = await new Promise(resolve => {
+    rl.question('Enter the HTTP method (GET, DELETE, PUT, or POST): ', answer => {
+      resolve(answer);
     });
-  
-    const method = await new Promise(resolve => {
-      rl.question('Enter the HTTP method (GET, DELETE, PUT, or POST): ', answer => {
-        resolve(answer);
-      });
+  });
+
+  const url = await new Promise(resolve => {
+    rl.question('Enter the URL: http://localhost:3000/api/v1/', answer => {
+      resolve("http://localhost:3000/api/v1/"+answer);
     });
-  
-    const url = await new Promise(resolve => {
-      rl.question('Enter the URL: ', answer => {
-        resolve(answer);
-      });
-    });
-  
-    if (method && url) {
-      let requestOptions = {
-        method: method.toUpperCase()
-      };
-  
-      if (method.toUpperCase() === 'PUT' || method.toUpperCase() === 'POST') {
-        const jsonPayload = await new Promise(resolve => {
-          rl.question('Enter the JSON payload: ', answer => {
-            resolve(answer);
-          });
+  });
+
+  if (method && url) {
+    let requestOptions = {
+      method: method.toUpperCase()
+    };
+
+    if (method.toUpperCase() === 'PUT' || method.toUpperCase() === 'POST') {
+      const jsonPayload = await new Promise(resolve => {
+        rl.question('Enter the JSON payload: ', answer => {
+          resolve(answer);
         });
-  
-        requestOptions.headers = {
-          'Content-Type': 'application/json'
-        };
-  
-        requestOptions.body = jsonPayload;
-      }
-  
-      rl.close();
-  
-      try {
-        const response = await fetch(url, requestOptions);
-        const contentType = response.headers.get('content-type');
-  
-        if (contentType.includes('application/json')) {
-          const jsonData = await response.json();
-          console.log('Response (JSON):', jsonData);
-        } else if (contentType.includes('application/xml')) {
-          const xmlData = await response.text();
-          console.log('Response (XML):', xmlData);
-        } else {
-          console.log('Unsupported content type:', contentType);
-        }
-      } catch (error) {
-        console.error('Error:', error);
-      }
-    } else {
-      rl.close();
-      console.log('Invalid input');
+      });
+
+      requestOptions.headers = {
+        'Content-Type': 'application/json'
+      };
+
+      requestOptions.body = jsonPayload;
     }
-  };
-  
-  fetchUrl();
+
+    rl.close();
+
+    try {
+      const response = await fetch(url, requestOptions);
+      const contentType = response.headers.get('content-type');
+
+      if (contentType.includes('application/json')) {
+        const jsonData = await response.json();
+        console.log('Response (JSON):', jsonData);
+      } else if (contentType.includes('application/xml')) {
+        const xmlData = await response.text();
+        console.log('Response (XML):', xmlData);
+      } else {
+        console.log('Unsupported content type:', contentType);
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  } else {
+    rl.close();
+    console.log('Invalid input');
+  }
+};
+
+fetchUrl();
   
